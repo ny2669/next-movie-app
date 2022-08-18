c207e7c5629b62599bf41c2e8236ecf274eba58b
ny jones <ny_8@hotmail.co.uk>
2022-08-18 08:31:55 +0100

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth"
+// import NextAuth from "next-auth"
 
-import GoogleProvider from "next-auth/providers/google";
+// import GoogleProvider from "next-auth/providers/google";
 
 
-export default NextAuth({
-    // Configure one or more authentication providers
-    providers: [
-        GoogleProvider({
-          clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-          clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+// export default NextAuth({
+//     // Configure one or more authentication providers
+//     providers: [
+//         GoogleProvider({
+//           clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+//           clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
          
-        })
-      ],
+//         })
+//       ],
      
-  })
+//   })
 
